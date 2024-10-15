beeac0bf31267699b52a931fe8426af812b994e8
Orlando Verdin <git@orli.land>
2024-10-15 14:52:03 -0700

    add product basic styles without button

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,21 @@
+import "./Products.css";
+import "./Product.css";
+
 const ProductCard = ({ data }) => {
   return (
-    <article>
-      <picture>
+    <article className="product">
+      <picture className="product__image">
         <source srcSet={data.image.desktop} media="(min-width: 1024px)" />
         <source srcSet={data.image.tablet} media="(min-width: 768px)" />
         <img src={data.image.mobile} alt={data.name} />
       </picture>
+      <div className="product__body">
+        <span className="product__category text__preset__4">
+          {data.category}
+        </span>
+        <h3 className="product__name text__preset__3">{data.name}</h3>
+        <span className="product__price text__preset__3">${data.price}</span>
+      </div>
     </article>
   );
 };
