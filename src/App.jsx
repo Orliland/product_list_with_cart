4721be1da5f0bfd48835225588960af9a4ce045e
Orlando Verdin <git@orli.land>
2024-10-15 08:05:58 -0700

    add empty cart element

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <main className="main">
       <header className="header">
-        <h1 className="logo text__preset__1--bold">Desserts</h1>
+        <h1 className="logo text__preset__1">Desserts</h1>
       </header>
       <section className="products">j</section>
       <Cart />
