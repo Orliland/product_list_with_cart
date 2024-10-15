5b59201a57566f52ace46e3b5ab1581ac5c70f9f
Orlando Verdin <git@orli.land>
2024-10-15 01:28:24 -0700

    Add custom variable of design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// Supports weights 300-700
+import "@fontsource-variable/red-hat-text";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
