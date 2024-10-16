f52f187f18aa6038ce0bfd6c1f0a568ff86b5aa4
Orlando Verdin <git@orli.land>
2024-10-16 08:25:34 -0700

    add control button and handle add remove product

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cartProducts, setCartProducts] = useState({});
 
   useEffect(() => {
     getProductsList();
@@ -18,12 +19,34 @@ const App = () => {
     setProducts(json);
   }
 
+  const handleAddToCart = (id) => {
+    if (cartProducts[id]) {
+      setCartProducts({ ...cartProducts, [id]: cartProducts[id] + 1 });
+    } else {
+      setCartProducts({ ...cartProducts, [id]: 1 });
+    }
+  };
+
+  const handleRemoveFromCart = (id) => {
+    if (cartProducts[id] == 1) {
+      const removeFromObject = delete cartProducts[id];
+      setCartProducts(removeFromObject);
+    } else {
+      setCartProducts({ ...cartProducts, [id]: cartProducts[id] - 1 });
+    }
+  };
+
   return (
     <main className="main">
       <header className="header">
         <h1 className="logo text__preset__1">Desserts</h1>
       </header>
-      <Products products={products} />
+      <Products
+        cartProducts={cartProducts}
+        products={products}
+        onAddToCart={handleAddToCart}
+        onRemoveFromCart={handleRemoveFromCart}
+      />
       <Cart />
     </main>
   );
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,35 +1,62 @@
-import { AddToCart } from "./Buttons";
+import { AddToCart, ControlCart } from "./Buttons";
 import "./Products.css";
 import "./Product.css";
 
-const ProductCard = ({ data }) => {
+const ProductCard = ({
+  product,
+  onAddToCart,
+  onRemoveFromCart,
+  cartProducts,
+}) => {
   return (
     <article className="product">
       <div className="product__header">
         <picture className="product__image">
-          <source srcSet={data.image.desktop} media="(min-width: 1024px)" />
-          <source srcSet={data.image.tablet} media="(min-width: 768px)" />
-          <img src={data.image.mobile} alt={data.name} />
+          <source srcSet={product.image.desktop} media="(min-width: 1024px)" />
+          <source srcSet={product.image.tablet} media="(min-width: 768px)" />
+          <img src={product.image.mobile} alt={product.name} />
         </picture>
-        <AddToCart />
+        {cartProducts[product.id] ? (
+          <ControlCart
+            id={product.id}
+            onAddToCart={onAddToCart}
+            cartCountProduct={cartProducts[product.id]}
+            onRemoveFromCart={onRemoveFromCart}
+          />
+        ) : (
+          <AddToCart id={product.id} onAddToCart={onAddToCart} />
+        )}
       </div>
 
       <div className="product__body">
         <span className="product__category text__preset__4">
-          {data.category}
+          {product.category}
         </span>
-        <h3 className="product__name text__preset__3">{data.name}</h3>
-        <span className="product__price text__preset__3">${data.price}</span>
+        <h3 className="product__name text__preset__3">{product.name}</h3>
+        <span className="product__price text__preset__3">${product.price}</span>
       </div>
     </article>
   );
 };
 
-const Products = ({ products }) => {
+const Products = ({
+  products,
+  onAddToCart,
+  onRemoveFromCart,
+  cartProducts,
+}) => {
   return (
     <section className="products">
       {products.map((product) => {
-        return <ProductCard data={product} key={product.id} />;
+        return (
+          <ProductCard
+            product={product}
+            key={product.id}
+            onAddToCart={onAddToCart}
+            cartProducts={cartProducts}
+            onRemoveFromCart={onRemoveFromCart}
+          />
+        );
       })}
     </section>
   );
